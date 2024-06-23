de00b43f77d2b835654450624e23b26683317d0b
maksymilianpadalak <maksymilian.padalak@gmail.com>
2024-06-24 00:53:01 +0200

    Create Heading test atom

---
+++ src/app/components/atoms/Heading/Heading.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState, FC, useEffect } from "react";
+
+export const Heading: FC = () => {
+  const [x, setX] = useState("We are Videotools!");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setX((prevState) => {
+        return prevState === "We are Videotools!"
+          ? "Hello!"
+          : "We are Videotools!";
+      });
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <h1>{x}</h1>;
+};
