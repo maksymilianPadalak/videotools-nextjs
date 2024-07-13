d6f9e89cd537258a643e711b58858b62b570cc12
maksymilianpadalak <maksymilian.padalak@gmail.com>
2024-07-13 18:16:21 +0200

    Create HeroImage component

---
+++ src/app/components/organisms/HeroImage/HeroImage.tsx
@@ -0,0 +1,39 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import { FC } from "react";
+
+type HeroImageProps = {
+  imageUrl: StaticImport;
+  title: string;
+  description: string;
+  imagePosition?: "left" | "right";
+};
+
+export const HeroImage: FC<HeroImageProps> = ({
+  imageUrl,
+  title,
+  description,
+  imagePosition = "left", // Default to 'left' if not specified
+}) => {
+  const imageContainerClasses =
+    imagePosition === "left" ? "lg:order-first" : "lg:order-last";
+
+  return (
+    <div className="flex flex-col lg:flex-row items-center justify-center h-[600px] xl:h-[800px]">
+      <div className={`w-full lg:w-1/2 ${imageContainerClasses} p-6`}>
+        <Image
+          src={imageUrl}
+          alt={title}
+          width={1200}
+          height={800}
+          className="rounded-lg"
+        />
+      </div>
+
+      <div className="w-full lg:w-1/2 p-6 flex flex-col justify-center items-center text-center">
+        <h1 className="text-6xl font-extrabold mb-4">{title}</h1>
+        <h2 className="text-2xl font-semibold">{description}</h2>
+      </div>
+    </div>
+  );
+};
--- src/app/components/templates/LandingPage/LandingPage.tsx
+++ src/app/components/templates/LandingPage/LandingPage.tsx
@@ -6,11 +6,14 @@ import Blachowicz from "@assets/BannerPhotos/Blachowicz.jpg";
 import Collection from "@assets/BannerPhotos/Collection.jpg";
 import Ostaszewska from "@assets/BannerPhotos/Ostaszewska.jpg";
 import Zewlakow from "@assets/BannerPhotos/Zewlakow.jpg";
+import Lewandowski from "@assets/BannerPhotos/Lewandowski.jpg";
 import { ImageProps } from "next/image";
+import { HeroImage } from "@organisms/HeroImage";
 
 const images: ImageProps[] = [
   { src: Benhacker, alt: "Benhacker" },
   { src: Blachowicz, alt: "Blachowicz" },
+  { src: Lewandowski, alt: "Lewandowski" },
   { src: Collection, alt: "Collection" },
   { src: Ostaszewska, alt: "Ostaszewska" },
   { src: Zewlakow, alt: "Zewlakow" },
@@ -29,7 +32,29 @@ export const LandingPage = () => {
     <div className="w-full">
       <LogoBanner images={images} />
       <TitleDescription title={title} description={description} />
-      <Mosaic />
+      <HeroImage
+        title="Antoni Padalak"
+        description="Antoni Padalak to utalentowany grafik, znany z kreatywnego podejścia oraz dbałości o szczegóły. Z pasją łączy innowacyjne podejście artystyczne z funkcjonalnymi zasadami projektowania, co pozwoliło mu zyskać uznanie w branży.
+        Swoją przygodę z grafiką rozpoczął z fascynacją opowiadaniem historii za pomocą obrazu oraz zobowiązaniem do tworzenia przekonujących narracji wizualnych. Przez lata doskonalił swoje umiejętności w typografii, projektowaniu układów oraz brandingowym, dbając o to, by każdy projekt miał mocny oddźwięk wśród jego odbiorców.
+        Prace Antoniego Padalaka obejmują różnorodne sektory, od tworzenia identyfikacji wizualnej i kampanii reklamowych po projektowanie edytorskie i sztukę cyfrową. Jego portfolio wyróżnia się różnorodnością i oryginalnym stylem, co czyni go cenionym specjalistą w dziedzinie grafiki."
+        imageUrl={Zewlakow}
+      />
+      <HeroImage
+        title="Antoni Padalak"
+        description="Antoni Padalak to utalentowany grafik, znany z kreatywnego podejścia oraz dbałości o szczegóły. Z pasją łączy innowacyjne podejście artystyczne z funkcjonalnymi zasadami projektowania, co pozwoliło mu zyskać uznanie w branży.
+        Swoją przygodę z grafiką rozpoczął z fascynacją opowiadaniem historii za pomocą obrazu oraz zobowiązaniem do tworzenia przekonujących narracji wizualnych. Przez lata doskonalił swoje umiejętności w typografii, projektowaniu układów oraz brandingowym, dbając o to, by każdy projekt miał mocny oddźwięk wśród jego odbiorców.
+        Prace Antoniego Padalaka obejmują różnorodne sektory, od tworzenia identyfikacji wizualnej i kampanii reklamowych po projektowanie edytorskie i sztukę cyfrową. Jego portfolio wyróżnia się różnorodnością i oryginalnym stylem, co czyni go cenionym specjalistą w dziedzinie grafiki."
+        imageUrl={Zewlakow}
+        imagePosition="right"
+      />
+      <HeroImage
+        title="Antoni Padalak"
+        description="Antoni Padalak to utalentowany grafik, znany z kreatywnego podejścia oraz dbałości o szczegóły. Z pasją łączy innowacyjne podejście artystyczne z funkcjonalnymi zasadami projektowania, co pozwoliło mu zyskać uznanie w branży.
+        Swoją przygodę z grafiką rozpoczął z fascynacją opowiadaniem historii za pomocą obrazu oraz zobowiązaniem do tworzenia przekonujących narracji wizualnych. Przez lata doskonalił swoje umiejętności w typografii, projektowaniu układów oraz brandingowym, dbając o to, by każdy projekt miał mocny oddźwięk wśród jego odbiorców.
+        Prace Antoniego Padalaka obejmują różnorodne sektory, od tworzenia identyfikacji wizualnej i kampanii reklamowych po projektowanie edytorskie i sztukę cyfrową. Jego portfolio wyróżnia się różnorodnością i oryginalnym stylem, co czyni go cenionym specjalistą w dziedzinie grafiki."
+        imageUrl={Zewlakow}
+      />
+      {/* <Mosaic /> */}
     </div>
   );
 };
