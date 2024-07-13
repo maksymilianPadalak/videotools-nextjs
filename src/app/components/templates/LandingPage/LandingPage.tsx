cea094c88657a0e4f95b3e65475223c6bb833955
maksymilianpadalak <maksymilian.padalak@gmail.com>
2024-07-13 13:57:56 +0200

    Add landing page template, update metadata

---
+++ src/app/components/templates/LandingPage/LandingPage.tsx
@@ -0,0 +1,10 @@
+import { Mosaic } from "../../organisms/Mosaic";
+
+export const LandingPage = () => {
+  return (
+    <div className="w-full">
+      <h1>This is landing page</h1>
+      <Mosaic />
+    </div>
+  );
+};
