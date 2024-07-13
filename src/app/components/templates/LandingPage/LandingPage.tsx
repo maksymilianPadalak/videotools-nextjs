52d30e47f53dec3b29b0e6a0ed5961f365fcd7db
maksymilianpadalak <maksymilian.padalak@gmail.com>
2024-07-13 17:22:37 +0200

    Move banner to landing page template

--- src/app/components/templates/LandingPage/LandingPage.tsx
+++ src/app/components/templates/LandingPage/LandingPage.tsx
@@ -1,8 +1,22 @@
 import { Mosaic } from "../../organisms/Mosaic";
+import { LogoBanner } from "@organisms/LogoBanner";
+import Benhacker from "@assets/BannerPhotos/Benhacker.jpg";
+import Blachowicz from "@assets/BannerPhotos/Blachowicz.jpg";
+import Collection from "@assets/BannerPhotos/Collection.jpg";
+import Ostaszewska from "@assets/BannerPhotos/Ostaszewska.jpg";
+import { ImageProps } from "next/image";
+
+const images: ImageProps[] = [
+  { src: Benhacker, alt: "Benhacker" },
+  { src: Blachowicz, alt: "Blachowicz" },
+  { src: Collection, alt: "Collection" },
+  { src: Ostaszewska, alt: "Ostaszewska" },
+];
 
 export const LandingPage = () => {
   return (
     <div className="w-full">
+      <LogoBanner images={images} />
       <h1>This is landing page</h1>
       <Mosaic />
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,6 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import cx from "classnames";
 import "./globals.css";
-import { LogoBanner } from "@organisms/LogoBanner";
-import Benhacker from "@assets/BannerPhotos/Benhacker.jpg";
-import Blachowicz from "@assets/BannerPhotos/Blachowicz.jpg";
-import Collection from "@assets/BannerPhotos/Collection.jpg";
-import Ostaszewska from "@assets/BannerPhotos/Ostaszewska.jpg";
-
-import { ImageProps } from "next/image";
 
 //TODO move font to font folder
 const openSans = Open_Sans({
@@ -21,13 +14,6 @@ export const metadata: Metadata = {
   description: "Photo, Video, 3D",
 };
 
-const images: ImageProps[] = [
-  { src: Benhacker, alt: "Benhacker" },
-  { src: Blachowicz, alt: "Blachowicz" },
-  { src: Collection, alt: "Collection" },
-  { src: Ostaszewska, alt: "Ostaszewska" },
-];
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,10 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cx(openSans.variable)}>
-        <LogoBanner images={images} />
-        {children}
-      </body>
+      <body className={cx(openSans.variable)}>{children}</body>
     </html>
   );
 }
