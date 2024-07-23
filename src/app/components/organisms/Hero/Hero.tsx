c766d71caa6dbca4ef11d04fe215acd5d84ca345
maksymilianpadalak <maksymilian.padalak@gmail.com>
2024-07-23 23:36:52 +0200

    Change Hero component description text

--- src/app/components/organisms/Hero/Hero.tsx
+++ src/app/components/organisms/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero: FC<HeroProps> = ({ title, description, className }) => {
   return (
     <div className={cx("flex flex-col justify-center items-center", className)}>
       <h1 className="text-3xl md:text-5xl lg:text-7xl">{title}</h1>
-      <p className="text-4xl md:text-5xl lg:text-6xl">{description}</p>
+      <p className="text-2xl md:text-3xl lg:text-4xl">{description}</p>
     </div>
   );
 };
