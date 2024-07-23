b1607248be30ae05d7287191f58bbc364f931f85
maksymilianpadalak <maksymilian.padalak@gmail.com>
2024-07-23 23:21:40 +0200

    Adjust Hero styling for mobile

--- src/app/components/organisms/Hero/Hero.tsx
+++ src/app/components/organisms/Hero/Hero.tsx
@@ -6,8 +6,8 @@ type HeroProps = { title: string; description: string; className?: string };
 export const Hero: FC<HeroProps> = ({ title, description, className }) => {
   return (
     <div className={cx("flex flex-col justify-center items-center", className)}>
-      <h1 className="text-9xl">{title}</h1>
-      <p className="text-4xl">{description}</p>
+      <h1 className="text-5xl md:text-7xl lg:text-9xl">{title}</h1>
+      <p className="text-4xl md:text-5xl lg:text-6xl">{description}</p>
     </div>
   );
 };
